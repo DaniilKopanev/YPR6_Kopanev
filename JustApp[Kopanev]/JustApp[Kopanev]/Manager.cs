9b01691d684cc1c14a2292b33ff7558d63924d90
DaniilKopanev <114130102+DaniilKopanev@users.noreply.github.com>
2022-11-19 11:19:17 +1000

    Add files via upload

---
+++ JustApp[Kopanev]/JustApp[Kopanev]/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace JustApp_Kopanev_
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
